ce98d56cc7065b8a4a1b9cab3f0ffec740340e16
danielfcarvalho <danielluisfc@gmail.com>
2023-08-17 21:17:00 +0100

    Fetching and adding supported currencies to the DB

--- backend/src/main/java/com/dfc/exchange_api/backend/models/Currency.java
+++ backend/src/main/java/com/dfc/exchange_api/backend/models/Currency.java
@@ -23,4 +23,9 @@ public class Currency {
 
     private String name;
     private String code;
+
+    public Currency(String name, String code) {
+        this.name = name;
+        this.code = code;
+    }
 }
